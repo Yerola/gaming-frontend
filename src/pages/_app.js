19456c941a442608c43c755368a56c47addfe256
Yesica Romina Lato <108998939+Yerola@users.noreply.github.com>
2023-04-26 10:40:04 -0300

    Delete src directory

--- src/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import 'semantic-ui-css/semantic.min.css'
-//After install, import the minified CSS file in your app's entry file:
-import '@/scss/global.scss'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
