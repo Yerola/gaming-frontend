08cd07d5961806f6d1913ac245008fe9c08f93c3
Yesica Romina Lato <108998939+Yerola@users.noreply.github.com>
2023-04-26 11:00:16 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import { AuthProvider, CartProvider } from "@/contexts";
+import { AuthProvider, CartProvider } from "/src/contexts";
 import "semantic-ui-css/semantic.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
