77bccfe50d9c5e54eae029aa82bc139a39933896
Yesica Romina Lato <108998939+Yerola@users.noreply.github.com>
2023-05-07 22:46:24 -0300

    Update sign-in.js

--- src/pages/join/sign-in/sign-in.js
+++ src/pages/join/sign-in/sign-in.js
@@ -3,16 +3,23 @@ import { JoinLayout } from "@/layouts";
 import { LoginForm } from "@/components/Auth";
 import { Seo } from "@/components/Shared";
 import styles from "./sign-in.module.scss";
+import { useSession,signIn } from "next-auth/react";
+
 
 export default function SignInPage() {
+  const{data:session}=useSession()
+
+
   return (
     <>
       <Seo title="Iniciar sesión" />
 
       <JoinLayout>
+
         <div className={styles.signIn}>
+          <button onClick={()=>signIn()}>iniciar sesion con google</button>
           <h3>Iniciar sesión</h3>
-          <LoginForm />
+          <LoginForm session={session}/>
 
           <div className={styles.actions}>
             <Link href="/join/sign-up">¿No tienes una cuenta?</Link>
