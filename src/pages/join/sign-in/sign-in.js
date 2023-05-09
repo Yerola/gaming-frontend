dde100b4896d92224cef728502391c91824845e8
AleDC27 <walteralejandrogordillo@gmail.com>
2023-05-09 01:05:40 -0300

    styles authenticador

--- src/pages/join/sign-in/sign-in.js
+++ src/pages/join/sign-in/sign-in.js
@@ -19,10 +19,11 @@ export default function SignInPage() {
           <h3>Iniciar sesión</h3>
 
           <div>
-            <Button className={styles.buttom2} onClick={() => signIn("google")}>
-              <FcGoogle size={25} /> Iniciar sesión con Google
+            <Button className={styles.buttom2} onClick={() => signIn("google")} style={{display:"flex",justifyContent:"space-around",alignItems:"center"}} >
+              <FcGoogle size={25}  style={{margin:"0 5px 0 0"}} /> <spam style={{color:"#555555"}} >Iniciar sesión con Google</spam>
             </Button>
           </div>
+          <br/>
 
           <LoginForm session={session} />
 
