011bf5d736c2d0f73a8924ad5d5dbf311867e71c
mcruzr85 <mcruzrodriguez1985@gmail.com>
2023-05-09 00:07:10 -0300

    autenticacion con Google

--- src/pages/join/sign-in/sign-in.js
+++ src/pages/join/sign-in/sign-in.js
@@ -2,32 +2,28 @@ import Link from "next/link";
 import { JoinLayout } from "@/layouts";
 import { LoginForm } from "@/components/Auth";
 import { Seo } from "@/components/Shared";
-import styles from "./sign-in.module.scss";
 import { useSession, signIn } from "next-auth/react";
-
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "semantic-ui-react";
+import styles from "./sign-in.module.scss";
 
 export default function SignInPage() {
-  const { data: session } = useSession()
-
-  const handleSignInWithGoogle = () => {
-    if (!session) {
-      // Si el usuario no ha iniciado sesión, mostrar una advertencia
-      alert("Debes registrarte primero antes de iniciar sesión con Google.");
-    } else {
-      // Si el usuario ha iniciado sesión, permitir que inicie sesión con Google
-      signIn("google");
-    }
-  };
+  const { data: session } = useSession();
 
   return (
     <>
       <Seo title="Iniciar sesión" />
 
       <JoinLayout>
-
         <div className={styles.signIn}>
-          <button className={styles.button} onClick={handleSignInWithGoogle}>Iniciar sesión con Google</button>
           <h3>Iniciar sesión</h3>
+
+          <div>
+            <Button className={styles.buttom2} onClick={() => signIn("google")}>
+              <FcGoogle size={25} /> Iniciar sesión con Google
+            </Button>
+          </div>
+
           <LoginForm session={session} />
 
           <div className={styles.actions}>
