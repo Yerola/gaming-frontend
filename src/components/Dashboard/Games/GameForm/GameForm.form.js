1aeaca1d947f3dd009a30454f2f31a9f14c6848c
LeoHeffel <leo_heffel@outlook.com>
2023-05-10 11:31:31 -0300

    fix(cambios ale)

--- src/components/Dashboard/Games/GameForm/GameForm.form.js
+++ src/components/Dashboard/Games/GameForm/GameForm.form.js
@@ -19,10 +19,11 @@ export function validationSchema() {
   return Yup.object({
     title: Yup.string()
       .required(msjError)
-      .matches(/^[a-zA-Z\s]+$/, 'Solo se permiten letras y espacios en blanco'),
+      .matches(/^[a-zA-Z0-9\s]+$/, 'Solo se permiten letras y numeros'),
     platform: Yup.number(),
     price: Yup.number().required(msjError),
-    discount: Yup.number(),
+    discount: Yup.number()
+    .max(100,"Debe ser un numero de 0 a 100"),
     summary: Yup.string().required(msjError),
     video: Yup.string().url().required(msjError),
     releaseDate: Yup.date().required(msjError),
