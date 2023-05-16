c0bfc6a068f0cf1950b8eaa55e021ec3d712127f
Yesica Romina Lato <108998939+Yerola@users.noreply.github.com>
2023-05-15 22:38:55 -0300

    Update ReviewForm.Form.js

--- src/components/Account/Orders/Order/reviewForm/ReviewForm.Form.js
+++ src/components/Account/Orders/Order/reviewForm/ReviewForm.Form.js
@@ -11,9 +11,11 @@ export function initialValues() {
 export function validationSchema() {
     return Yup.object({
     review: Yup.string()
+      .max(400, "El campo no puede tener más de 315 caracteres")
         .required("Campo obligatorio"),
     rating:Yup.number()
     .max(5,"debe ser un numero entre 1 a 5")
+      .min(1,"debe ser un numero entre 1 a 5")
     .required("Campo obligatorio")
     });
   }
