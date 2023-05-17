dc87b2cba9e272beb656a14761c79a8d6f4f9a48
Yesica Romina Lato <ye_lato@yahoo.com.ar>
2023-05-16 23:12:02 -0300

    page-404 v.2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,34 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './404.module.scss';
 
 const NotFoundPage = () => {
   const router = useRouter();
 
   return (
-    <div>
+    <div className={styles['not-found-page']}>
+      <img
+        src="https://gaming-frontend.vercel.app/images/logo.png"
+        alt="GAMING"
+        style={{
+          position: 'absolute',
+          top: '23.5%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '500px',
+          height: '90px'
+        }}
+      />
       <h1>Oops! Página no encontrada</h1>
-      <p>La página que estás buscando no existe.</p>
+      <hr className={styles.hr} />
+      <p className={styles.p}>La página que estás buscando no existe.</p>
+      <hr className={styles.hr} />
       <Link href="/" passHref>
-        <div className="link-wrapper">
+        <div className={styles['link-wrapper']}>
           <a>Volver a la página de inicio</a>
         </div>
       </Link>
-      <button onClick={() => router.back()}>Volver a la página anterior</button>
+      <button className={styles.backButton} onClick={() => router.back()}>Volver a la página anterior</button>
     </div>
   );
 };
