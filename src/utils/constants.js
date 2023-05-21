41bca8e9821a3317114ac8e09d0a1231fa361e7c
Yesica Romina Lato <ye_lato@yahoo.com.ar>
2023-05-21 19:36:04 -0300

    fix-spinner-dashboard-admin-ventas

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const ENV = {
   //SERVER_HOST: "http://127.0.0.1:1337",
   //API_URL: "http://127.0.0.1:1337/api",
   ENDPOINTS: {
-    AUTH: {   
+    AUTH: {
       REGISTER: "auth/local/register",
       LOGIN: "auth/local",
     },
